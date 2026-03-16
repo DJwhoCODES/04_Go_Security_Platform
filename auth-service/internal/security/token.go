3aff3bdd4425d37841684ee7e974036aa1b73017
DJwhoCODES <jaindevanshu824@gmail.com>
2026-03-17 00:07:30 +0530

    token generation

--- auth-service/internal/security/token.go
+++ auth-service/internal/security/token.go
@@ -1,1 +1,18 @@
 package security
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+func GenerateRandomToken(length int) (string, error) {
+
+	b := make([]byte, length)
+
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.RawURLEncoding.EncodeToString(b), nil
+}
